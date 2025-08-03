2ce414b5e8380e830a7b7a653c9707608792950d
michael lima <michael.lima.dev@gmail.com>
2025-08-03 16:19:52 -0300

    first steps in java with spring boot 3.4.0

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/michael/Startup.java
@@ -0,0 +1,13 @@
+package br.com.michael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
